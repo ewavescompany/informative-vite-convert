33051ffd31ccc10cb29341110c2fda79ce8f1a60
adel <ma7598101@gmail.com>
2024-11-11 14:39:56 +0200

    INITIATE: vite project have (shadcn - tailwind - react-router-dom)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">hello there</h1>
+      <Button>click me</Button>
+    </div>
+  );
+}
+
+export default App;
