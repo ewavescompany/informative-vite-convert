834d94954108532e06712274b56a8cf378f1a2a8
adel <ma7598101@gmail.com>
2024-12-05 14:04:54 +0200

    TEST: hostinger auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,6 @@ function App() {
         <meta name="description" content="ewavespro website" />
         <link rel="icon" href={icon} type="image/x-icon" />
       </Helmet> */}
-      <h1>hello</h1>
-      <h2>another test</h2>
-      <h3>another test</h3>
       <Router future={{ v7_startTransition: true }}>
         <AppRoutes />
       </Router>
